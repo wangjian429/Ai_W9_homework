30b20ba4f450892874a33c9f4551430bf764813b
wangjian429 <40121751+wangjian429@users.noreply.github.com>
2018-07-28 13:47:18 +0800

    Update inference.py

--- research/inference.py
+++ research/inference.py
@@ -71,5 +71,6 @@ if __name__ == '__main__':
                 np.squeeze(scores),
                 category_index,
                 use_normalized_coordinates=True,
+                min_score_thresh=.2,
                 line_thickness=8)
             plt.imsave(os.path.join(FLAGS.output_dir, 'output.png'), image_np)
